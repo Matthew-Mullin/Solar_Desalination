9a8f37d6bc32ac687905372858af0d988efde9af
Matthew-Mullin <matthewmullin.mm@gmail.com>
2021-04-06 08:41:00 -0400

    Create psuedocode.ino
    made psuedocode file

---
+++ psuedocode/psuedocode.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  //define pressure sensor 1
+  //define pressure sensor 2
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //if pressure sensor 1<2 {turn on motor 1}
+}
