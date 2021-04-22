ec655ec806bb08cdd45ed80ac8d318078c8e40f5
ChrisY54 <80995588+ChrisY54@users.noreply.github.com>
2021-04-22 19:58:20 +0900

    Update stepper.h

--- stepper.h
+++ stepper.h
@@ -1,60 +1,39 @@
-/*
- * Simple demo, should work with any driver board
- *
- * Connect STEP, DIR as indicated
- *
- * Copyright (C)2015-2017 Laurentiu Badea
- *
- * This file may be redistributed under the terms of the MIT license.
- * A copy of this license has been included with this distribution in the file LICENSE.
- */
-#include <Arduino.h>
-#include "BasicStepperDriver.h"
+// Define pin connections & motor's steps per revolution
+const int dirPin = 2;
+const int stepPin = 3;
+const int stepsPerRevolution = 200;
 
-// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
-#define MOTOR_STEPS 200
-#define RPM 120
-
-// Since microstepping is set externally, make sure this matches the selected mode
-// If it doesn't, the motor will move at a different RPM than chosen
-// 1=full step, 2=half step etc.
-#define MICROSTEPS 1
-
-// All the wires needed for full functionality
-#define DIR 8
-#define STEP 9
-//Uncomment line to use enable/disable functionality
-//#define SLEEP 13
-
-// 2-wire basic config, microstepping is hardwired on the driver
-BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP);
-
-//Uncomment line to use enable/disable functionality
-//BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP, SLEEP);
-
-void setup() {
-    stepper.begin(RPM, MICROSTEPS);
-    // if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next line
-    // stepper.setEnableActiveState(LOW);
+void setup()
+{
+	// Declare pins as Outputs
+	pinMode(stepPin, OUTPUT);
+	pinMode(dirPin, OUTPUT);
 }
+void loop()
+{
+	// Set motor direction clockwise
+	digitalWrite(dirPin, HIGH);
 
-void loop() {
-  
-    // energize coils - the motor will hold position
-    // stepper.enable();
-  
-    /*
-     * Moving motor one full revolution using the degree notation
-     */
-    stepper.rotate(360);
+	// Spin motor slowly
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(2000);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(2000);
+	}
+	delay(1000); // Wait a second
+	
+	// Set motor direction counterclockwise
+	digitalWrite(dirPin, LOW);
 
-    /*
-     * Moving motor to original position using steps
-     */
-    stepper.move(-MOTOR_STEPS*MICROSTEPS);
-
-    // pause and allow the motor to be moved by hand
-    // stepper.disable();
-
-    delay(5000);
+	// Spin motor quickly
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(1000);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(1000);
+	}
+	delay(1000); // Wait a second
 }
